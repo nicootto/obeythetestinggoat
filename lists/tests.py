0768e07f67fd0bc7ed900673cbe624633110347e
Nicolas Ottonello <nicolas.ottonello96@gmail.com>
2019-12-22 10:46:15 -0300

    Functional test we can insert multiple items in our to-do list

--- lists/tests.py
+++ lists/tests.py
@@ -1,15 +1,13 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
