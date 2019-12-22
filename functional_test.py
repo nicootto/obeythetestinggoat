2cf7e9c7cd17436a709c510b40f73e913b981b0f
Nicolas Ottonello <nicolas.ottonello96@gmail.com>
2019-12-22 11:31:25 -0300

    Model for list Items and associated migration

--- functional_test.py
+++ functional_test.py
@@ -42,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        
+
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
